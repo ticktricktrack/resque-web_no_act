b47512a551f293bbbaae8c843755d7b38612ca85
Rainer Kuhn <ticktricktrack@gmail.com>
2012-09-03 17:04:04 +0100

    override usual methods

--- lib/resque/server_no_act.rb
+++ lib/resque/server_no_act.rb
@@ -5,92 +5,32 @@ end
 module Resque
   class ServerNoAct < Server
 
-    def show(page, layout = true)
-      response["Cache-Control"] = "max-age=0, private, must-revalidate"
-      begin
-        erb page.to_sym, {:layout => layout}, :resque => Resque
-      rescue Errno::ECONNREFUSED
-        erb :error, {:layout => false}, :error => "Can't connect to Redis! (#{Resque.redis_id})"
-      end
-    end
-
-    def show_for_polling(page)
-      content_type "text/html"
-      @polling = true
-      show(page.to_sym, false).gsub(/\s{1,}/, ' ')
-    end
-
-    get "/no_act" do
+    def show_access_denied
       erb File.read(File.join(File.dirname(__FILE__), "server_no_act/views/no_act.erb"))
     end
 
-    # to make things easier on ourselves
-    get "/?" do
-      redirect url_path(:overview)
-    end
-
-    %w( overview workers ).each do |page|
-      get "/#{page}.poll/?" do
-        show_for_polling(page)
-      end
-
-      get "/#{page}/:id.poll/?" do
-        show_for_polling(page)
-      end
-    end
-
-    %w( overview queues working workers key ).each do |page|
-      get "/#{page}/?" do
-        show page
-      end
-
-      get "/#{page}/:id/?" do
-        show page
-      end
+    get "/no_act" do
+      show_access_denied
     end
 
     post "/queues/:id/remove" do
-      Resque.remove_queue(params[:id])
-      redirect u('queues')
-    end
-
-    get "/failed/?" do
-      if Resque::Failure.url
-        redirect Resque::Failure.url
-      else
-        show :failed
-      end
+      redirect u('no_act')
     end
 
     post "/failed/clear" do
-      Resque::Failure.clear
-      redirect u('failed')
+      redirect u('no_act')
     end
 
     post "/failed/requeue/all" do
-      Resque::Failure.count.times do |num|
-        Resque::Failure.requeue(num)
-      end
-      redirect u('failed')
+      redirect u('no_act')
     end
 
     get "/failed/requeue/:index/?" do
-      # Resque::Failure.requeue(params[:index])
       redirect u('no_act')
     end
 
     get "/failed/remove/:index/?" do
-      # Resque::Failure.remove(params[:index])
       redirect u('no_act')
-    end
-
-    # Helper method stolen from rescue-retry.
-    module Helpers
-      # reads a 'local' template file.
-      def local_template(path)
-        # Is there a better way to specify alternate template locations with sinatra?
-        File.read(File.join(File.dirname(__FILE__), "server_no_act/views/#{path}"))
-      end
     end
   end
 end
